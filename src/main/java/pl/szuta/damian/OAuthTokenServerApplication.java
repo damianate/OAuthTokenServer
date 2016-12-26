ceae92bf772d16a33a82a315b54345f003a64b3b
Damian Szuta <damianszuta@MacBook-Pro.local>
2016-12-26 10:44:33 +0100

    Generowanie tokenu za pomoca:
    curl pluralsight:secret@localhost:9000/api/oauth/token -d grant_type=password -d username=richard -d password=password| python -m json.tool

---
+++ src/main/java/pl/szuta/damian/OAuthTokenServerApplication.java
@@ -0,0 +1,14 @@
+package pl.szuta.damian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+public class OAuthTokenServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OAuthTokenServerApplication.class, args);
+	}
+}
